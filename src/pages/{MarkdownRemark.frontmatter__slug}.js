f152198700bf2fe06c6da5b99bbe33f293b94fac
Rishi Raval <ushir.raval@gmail.com>
2021-08-23 21:32:49 +0200

    Added highlightjs

--- src/pages/{MarkdownRemark.frontmatter__slug}.js
+++ src/pages/{MarkdownRemark.frontmatter__slug}.js
@@ -1,12 +1,18 @@
 import React from "react"
+import { useEffect } from "react"
 import { graphql } from "gatsby"
 import { Navbar } from "./navbar"
+import highlightCode from './syntaxHighlight'
+
 
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
 }) {
-  const { markdownRemark } = data // data.markdownRemark holds your post data
-  const { frontmatter, html } = markdownRemark
+  const { markdownRemark } = data; // data.markdownRemark holds your post data
+  const { frontmatter, html } = markdownRemark;
+  useEffect(() => {
+    highlightCode()
+  });
   return (
     <main>
       <title>U Raval | {frontmatter.title}</title>
